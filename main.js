ce5ee8c61c3c9f8c1065c9f78a7ac49e941ebe11
Sulphurologist <165383526+Sulphurologist@users.noreply.github.com>
2026-01-30 11:45:53 +0100

    Add GeoJSON source and layer with popups to map

--- main.js
+++ main.js
@@ -4,3 +4,31 @@ const map = new maplibregl.Map({
   center: [0, 20],
   zoom: 2
 });
+
+#load GeoJSON into MapLibre
+map.on('load', () => {
+  map.addSource('points', {
+    type: 'geojson',
+    data: 'data/vectors/points.geojson'
+  });
+
+  map.addLayer({
+    id: 'points-layer',
+    type: 'circle',
+    source: 'points',
+    paint: {
+      'circle-radius': 6,
+      'circle-color': '#ff0000'
+    }
+  });
+});
+
+#Popups
+map.on('click', 'points-layer', (e) => {
+  const props = e.features[0].properties;
+
+  new maplibregl.Popup()
+    .setLngLat(e.lngLat)
+    .setHTML(`<strong>${props.name}</strong><br>Value: ${props.value}`)
+    .addTo(map);
+});
